e9ccee01684988ea200af953f340e4cd47f44d91
AREUM AN <areum.an.0928@gmail.com>
2020-04-29 23:53:54 -0400

    Add Header for test

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import Router from './Router';
+import Header from './Header';
 
 const App = () => {
-  return <Router />;
+  return (
+    <>
+      <Header />
+      <Router />
+    </>
+  );
 };
 
 export default App;
---
+++ src/Components/Header.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header>
+      <ul>
+        <a href="/">HOME</a>
+      </ul>
+      <ul>
+        <a href="/movie">Movie</a>
+      </ul>
+      <ul>
+        <a href="/tv">TV</a>
+      </ul>
+      <ul>
+        <a href="/search">Search</a>
+      </ul>
+    </header>
+  );
+};
+
+export default Header;
