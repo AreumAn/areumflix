c70d90b70944c95f6377cc1b1c0f8c375d868aec
AREUM AN <areum.an.0928@gmail.com>
2020-04-29 22:53:30 -0400

    Test api call

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { movieApi } from './api';
 
-function App() {
-  return (
-    <div>TEST</div>
-  );
-}
+const App = () => {
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const {
+          data: { result: upcomingMovies },
+        } = await movieApi.nowPlaying();
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchData();
+  });
+  return <div>TEST</div>;
+};
 
 export default App;
